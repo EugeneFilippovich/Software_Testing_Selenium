5c553d721a3b216f25076ba7bf99dfdbbf487def
Eugene Filippovich <eugene.filippovich93@gmail.com>
2018-06-04 09:27:27 +0300

    test

--- Main/test_file.py
+++ Main/test_file.py
@@ -1,5 +1,16 @@
+import string
+import random
 import pytest
 from selenium import webdriver
+
+
+def id_generator(size=6, chars=string.ascii_lowercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
+
+
+var = id_generator()
+
+print(var)
 
 
 @pytest.fixture
@@ -14,53 +25,34 @@ def test_task_10(driver):
     :type driver: selenium.webdriver.Chrome
     """
 
-
     def log_in():
         driver.get("http://localhost/litecart/")
 
     log_in()
 
-    category = driver.find_element_by_id("box-campaigns")
-    item_link = category.find_element_by_class_name('link')
+    login_form = driver.find_element_by_css_selector('[name = login_form]')
+    new_user = login_form.find_element_by_tag_name('a')
+    new_user.click()
 
-    # item name
-    MP_item_name = item_link.find_element_by_class_name('name').get_attribute('textContent')
+    registration_form = driver.find_element_by_css_selector('[name = customer_form')
+    first_name = registration_form.find_element_by_name('firstname')
+    second_name = registration_form.find_element_by_name('lastname')
+    address_1 = registration_form.find_element_by_name('address1')
+    postcode = registration_form.find_element_by_name('postcode')
+    city = registration_form.find_element_by_name('city')
+    country = registration_form.find_element_by_css_selector('[role = combobox]')
+    zone_state_province = registration_form.find_element_by_name('zone_code')
+    emain = registration_form.find_element_by_name('email')
+    phone = registration_form.find_element_by_name('phone')
+    desired_password = registration_form.find_element_by_name('password')
+    confirm_password = registration_form.find_element_by_name('confirmed_password')
 
-    # regular-price line text and price check
-    MP_regular_price_font_line = item_link.find_element_by_tag_name("s").get_attribute('textContent')
-
-    # regular-price color check
-    MP_regular_price_color = driver.find_element_by_class_name('regular-price').value_of_css_property("color")
-
-    # campaign-price strong text and price check
-    MP_campaign_price_strong = item_link.find_element_by_tag_name("strong").get_attribute('textContent')
-
-    # campaign-price color check
-    MP_campaign_price_strong = driver.find_element_by_class_name('campaign-price').value_of_css_property("color")
-    item_link.click()
-
-
-    driver.get("http://localhost/litecart/en/rubber-ducks-c-1/subcategory-c-2/yellow-duck-p-1")
-
-    item_box = driver.find_element_by_id('box-product')
-
-    # item-page item name
-    IP_item_name = item_box.find_element_by_class_name("title").get_attribute('textContent')
-
-    # regular-price line text and price check
-    IP_item_regular_price = item_box.find_element_by_tag_name("s").get_attribute('textContent')
-
-    # regular-price color check
-    IP_regular_price_color = driver.find_element_by_class_name('regular-price').value_of_css_property("color")
-
-    # campaign-price strong text and price check
-    IP_campaign_price_strong = item_box.find_element_by_tag_name("strong").get_attribute('textContent')
-
-    # campaign-price color check
-    IP_campaign_price_strong = driver.find_element_by_class_name('campaign-price').value_of_css_property("color")
-
-
-
-
-
-
+    first_name.send_keys('Eugene')
+    second_name.send_keys('Filippovich')
+    address_1.send_keys('Mihalova str.')
+    postcode.send_keys(55555)
+    city.send_keys('Minsk')
+    emain.send_keys(var + "@gmail.com")
+    phone.send_keys('375291112233')
+    desired_password.send_keys(123123)
+    confirm_password.send_keys(123123)
