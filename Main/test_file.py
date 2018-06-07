8a37c69a3118e34526db07ea4dab75c46238b727
Eugene Filippovich <eugene.filippovich93@gmail.com>
2018-06-07 18:21:47 +0300

    task13 test

--- Main/test_file.py
+++ Main/test_file.py
@@ -6,6 +6,8 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 import time
 
+# С глобалом, да. Как-то без ООП можно было сделать?
+cart_items_quantity = 1
 
 @pytest.fixture
 def driver(request):
@@ -21,36 +23,74 @@ def test_task_10(driver):
 
     wait = WebDriverWait(driver, 10)
 
+
     def log_in():
         driver.get("http://localhost/litecart/")
 
+
     def wait_cart_update(element):
-        cart_items_quantity = 1
+        global cart_items_quantity
         try:
-            element = wait.until(lambda d: element.find_element_by_partial_link_text(str(cart_items_quantity)))
-            cart_items_quantity += 1
-            print(cart_items_quantity)
+            element = wait.until(lambda d: element.find_element_by_xpath('//span[starts-with(text(), {})]'.format(str(cart_items_quantity))))
+            if cart_items_quantity < 3:
+                cart_items_quantity += 1
+            else:
+                pass
         except ValueError:
             print('Item has not been added to the cart!')
 
+    def add_item_to_cart():
+        most_popular = driver.find_element_by_id('box-most-popular')
+        most_popular_items = most_popular.find_elements_by_class_name('link')
+        most_popular_items[0].click()
+
+        product_box = driver.find_element_by_id('box-product')
+        add_to_cart = product_box.find_element_by_css_selector('[name = add_cart_product]')
+        try:
+            size_selection = Select(product_box.find_element_by_name('options[Size]'))
+            size_selection.select_by_visible_text('Small')
+        except Exception:
+            pass
+
+        add_to_cart.click()
+
+        cart = driver.find_element_by_id('cart')
+        wait_cart_update(cart)
+
+
+
     log_in()
 
-    most_popular = driver.find_element_by_id('box-most-popular')
-    most_popular_items = most_popular.find_elements_by_class_name('link')
-    most_popular_items[0].click()
+    add_item_to_cart()
 
-    product_box = driver.find_element_by_id('box-product')
-    add_to_cart = product_box.find_element_by_css_selector('[name = add_cart_product]')
-    try:
-        size_selection = Select(product_box.find_element_by_name('options[Size]'))
-        size_selection.select_by_visible_text('Small')
-    except Exception:
-        pass
+    log_in()
 
-    add_to_cart.click()
+    add_item_to_cart()
 
-    cart = driver.find_element_by_id('cart')
-    wait_cart_update(cart)
+    log_in()
+
+    add_item_to_cart()
+
+    driver.find_element_by_link_text('Checkout »').click()
+    print(cart_items_quantity)
+    time.sleep(3)
+
+
+
+    table = driver.find_element_by_id('box-checkout-summary')
+    lines = table.find_elements_by_css_selector('tr:not(.header):not(.footer)')
+
+    for line in lines:
+        item_line = line.find_element_by_xpath('.//td[contains(text(), item)]')
+        print(item_line)
+        # remove_button = driver.find_element_by_name('remove_cart_item')
+        # remove_button.click()
+        # time.sleep(2)
+        # cart_items_quantity -= 1
+
+
+
+
 
 
 
