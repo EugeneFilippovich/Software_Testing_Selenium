27b066a608b41b8794668a44a0311b6545d8b631
Eugene Filippovich <eugene.filippovich93@gmail.com>
2018-06-19 12:01:51 +0300

    final task final

--- task13_high/tests.py
+++ task13_high/tests.py
@@ -1,34 +1,30 @@
-import pytest
-from task13_high.fixture import app
-from task13_high.app.application import Application
-
 # Работает
-a = Application()
-a.main_page_load()
-a.item_select()
-a.item_to_cart()
-a.main_page_load()
-a.item_select()
-a.item_to_cart()
-a.main_page_load()
-a.item_select()
-a.item_to_cart()
-a.checkout_items()
-a.clear_cart()
-
-a.quit()
+# a = Application()
+# a.main_page_load()
+# a.item_select()
+# a.item_to_cart()
+# a.main_page_load()
+# a.item_select()
+# a.item_to_cart()
+# a.main_page_load()
+# a.item_select()
+# a.item_to_cart()
+# a.checkout_items()
+# a.clear_cart()
+#
+# a.quit()
 
 
 # Не работает
-# def test_app(app):
-#     app.main_page_load()
-#     app.item_select()
-#     app.item_to_cart()
-#     app.main_page_load()
-#     app.item_select()
-#     app.item_to_cart()
-#     app.main_page_load()
-#     app.item_select()
-#     app.item_to_cart()
-#     app.checkout_items()
-#     app.clear_cart()
+def test_app(app):
+    app.main_page_load()
+    app.item_select()
+    app.item_to_cart()
+    app.main_page_load()
+    app.item_select()
+    app.item_to_cart()
+    app.main_page_load()
+    app.item_select()
+    app.item_to_cart()
+    app.checkout_items()
+    app.clear_cart()
