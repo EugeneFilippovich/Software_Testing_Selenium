462dccb2b859a095523513e8272fea3fc50f3eaf
Eugene Filippovich <eugene.filippovich93@gmail.com>
2018-06-19 12:02:18 +0300

    final task final

--- task13_high/tests.py
+++ task13_high/tests.py
@@ -1,21 +1,6 @@
-# Работает
-# a = Application()
-# a.main_page_load()
-# a.item_select()
-# a.item_to_cart()
-# a.main_page_load()
-# a.item_select()
-# a.item_to_cart()
-# a.main_page_load()
-# a.item_select()
-# a.item_to_cart()
-# a.checkout_items()
-# a.clear_cart()
-#
-# a.quit()
 
 
-# Не работает
+
 def test_app(app):
     app.main_page_load()
     app.item_select()
